90448df7517ec61072301e802f57fcb9d7897d57
XR-Y <191250187@smail.nju.edu.cn>
2021-11-21 13:11:06 +0800

    final

--- stanford-corenlp-full-2018-10-05/start.bat
+++
@@ -1,1 +0,0 @@
-java -Xmx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -serverProperties StanfordCoreNLP-chinese.properties -preload tokenize,ssplit,pos,lemma,ner,parse,depparse -status_port 9001 -port 9001 -timeout 15000
